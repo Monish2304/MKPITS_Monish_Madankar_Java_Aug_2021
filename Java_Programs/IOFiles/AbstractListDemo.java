abdac46ff39002f7a2d428495bc81772db4d1dde
Monish2304 <madankarmonish@gmail.com>
2021-10-23 11:33:17 +0530

    Added more files

---
+++ Java_Programs/IOFiles/AbstractListDemo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+  
+public class AbstractListDemo {
+    public static void main(String args[])
+    {
+  
+        // Creating an empty AbstractList
+        AbstractList<String> list = new ArrayList<String>();
+  
+        // Use add() method to add elements in the list
+        list.add("Geeks");
+        list.add("for");
+        list.add("mkpits");
+        list.add("10");
+        list.add("20");
+  
+        // Displaying the AbstractList
+        System.out.println("AbstractList:" + list);
+    }
+}
