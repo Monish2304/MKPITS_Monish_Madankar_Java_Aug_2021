e4098e222e731cb6f5db11e8ccf38af515dc5f28
Monish2304 <madankarmonish@gmail.com>
2021-12-23 10:40:51 +0530

    added file

---
+++ Java_Programs/IOFiles/CompareToExample.java
@@ -0,0 +1,36 @@
+/*public class CompareToExample {
+	public static void main(String []args){
+		String s1="hello";
+		String s2="hello";
+		System.out.println(s1.compareTo(s2));
+	}
+}*/
+
+
+/*public class CompareToExample {
+	public static void main(String []args){
+		String s1="hellow";
+		String s2="hello";
+		System.out.println(s1.compareTo(s2));
+	}
+}*/
+
+
+/*public class CompareToExample {
+	public static void main(String []args){
+		String s1="hello";
+		String s2="hellow";
+		System.out.println(s1.compareTo(s2));
+	}
+}*/
+
+
+
+public class CompareToExample {
+	public static void main(String []args){
+		String a="Madam";
+		String b="";
+
+		System.out.println(a.compareTo(b));
+	}
+}
