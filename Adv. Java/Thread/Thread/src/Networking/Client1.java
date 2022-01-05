c663eb4451a0431d43839b7b688232df50de27a0
Monish2304 <madankarmonish@gmail.com>
2022-01-05 09:31:54 +0530

    added new file

--- Adv. Java/Thread/Thread/src/Networking/Client1.java
+++ Adv. Java/Thread/Thread/src/Networking/Client1.java
@@ -1,11 +1,16 @@
 package Networking;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
-
-public class Client1 {
-    public static void main(String[] args) throws IOException {
-        Socket sob=new Socket("localhost",8000);
+import java.io.*;
+import java.net.*;
+class Client2 {
+    public static void main(String[] args) {
+        try{
+            Socket s=new Socket("localhost",6666);
+/*DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+dout.writeUTF("Hello Server");
+dout.flush();
+dout.close();
+s.close();*/
+        }catch(Exception e){System.out.println(e);}
     }
 }
